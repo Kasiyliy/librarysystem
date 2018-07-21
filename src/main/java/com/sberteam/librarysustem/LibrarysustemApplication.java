5af6adaff2bd1b3cbb17111730c1e54e6e8c54e1
air <asilkhan-al@mail.ru>
2018-07-21 19:11:08 +0600

    First commit Library system For libraries of Astana city V: 0.1

---
+++ src/main/java/com/sberteam/librarysustem/LibrarysustemApplication.java
@@ -0,0 +1,12 @@
+package com.sberteam.librarysustem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysustemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarysustemApplication.class, args);
+	}
+}
