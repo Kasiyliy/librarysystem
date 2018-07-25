5250b0a28bc4327d521ff2175bf5e5f4ec9ed970
ersultanbek <ersultan_elgondin@mail.ru>
2018-07-25 23:18:42 +0600

    Теперь работает авторизация и шифрование пароля

--- src/main/java/com/sberteam/librarysustem/models/AuthenticableUsers.java
+++ src/main/java/com/sberteam/librarysustem/models/AuthenticableUsers.java
@@ -1,5 +1,7 @@
 package com.sberteam.librarysustem.models;
 
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.util.Date;
 import java.util.Set;
@@ -7,7 +9,9 @@ import java.util.Set;
 @Entity
 public class AuthenticableUsers extends BasicUsers {
 
+
     private String username;
+
     private String password;
 
     public AuthenticableUsers() {
